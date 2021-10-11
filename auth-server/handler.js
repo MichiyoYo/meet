a3c8edd503984c36af69cbb3ad7ce8481bcff843
Cristina Lester <cristina.deelester@gmail.com>
2021-10-11 13:55:51 -0700

    updated the handlers

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -46,11 +46,11 @@ module.exports.getAuthURL = async () => {
 };
 
 module.exports.getAccessToken = async (event) => {
-  // const oAuth2Client = new google.auth.OAuth2(
-  //   client_id,
-  //   client_secret,
-  //   redirect_uris[0]
-  // );
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
@@ -82,11 +82,11 @@ module.exports.getAccessToken = async (event) => {
 };
 
 module.exports.getCalendarEvents = async (event) => {
-  // const oAuth2Client = new google.auth.OAuth2(
-  //   client_id,
-  //   client_secret,
-  //   redirect_uris[0]
-  // );
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   const access_token = decodeURIComponent(
     `${event.pathParameters.access_token}`
