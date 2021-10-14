825c13109e0282920dd1638ff4328bd45405b963
Cristina Lester <cristina.deelester@gmail.com>
2021-10-14 10:29:51 -0700

    added integration tests for citysearch and app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ import CitySearch from "./CitySearch";
 import { mockData } from "./mock-data";
 import { extractLocations } from "./api";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents } from "./api";
 
 class App extends Component {
+  locations = extractLocations(mockData);
   constructor(props) {
     super(props);
+
     this.state = {
       events: mockData,
+      locations: this.locations,
     };
   }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter(
+        (event) => event.location === location
+      );
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <CitySearch locations={extractLocations(mockData)} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <NumberOfEvents />
         <EventList events={this.state.events} />
       </div>
