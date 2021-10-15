a7c770ad0a4127d10491c5459b77f75c1981d27a
Cristina Lester <cristina.deelester@gmail.com>
2021-10-14 17:53:41 -0700

    roll back

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import axios from "axios";
 import { mockData } from "./mock-data";
 import { NProgress } from "nprogress";
@@ -66,7 +68,7 @@ export const getEvents = async () => {
   }
 
   const token = await getAccessToken();
-
+  console.log(token);
   if (token) {
     removeQuery();
     const url =
