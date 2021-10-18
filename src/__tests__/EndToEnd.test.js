2bb20b427482bc4079b9bc478e307b8c57e3dc11
Cristina Lester <cristina.deelester@gmail.com>
2021-10-18 10:05:36 -0700

    added end to end testing for scenario 1 and 2 of feature 2

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,30 @@
+import puppeteer from "puppeteer";
+
+describe("show/hide an event details", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .extra-details.hide");
+    expect(eventDetails).toBeDefined();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    await page.evaluate(() =>
+      document.querySelector(".show-details-btn").click()
+    );
+    const eventDetails = await page.$(".event .extra-details.show");
+    expect(eventDetails).toBeDefined();
+  });
+});
