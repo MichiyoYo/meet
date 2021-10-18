d6db43a06fe0bd09dcb0c4cef615db55d8d2deda
Cristina Lester <cristina.deelester@gmail.com>
2021-10-18 16:33:11 -0700

    changed order of tests

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,46 +1,6 @@
 import puppeteer from "puppeteer";
 
-describe("show/hide an event details", () => {
-  let browser;
-  let page;
-  beforeAll(async () => {
-    jest.setTimeout(30000);
-    browser = await puppeteer.launch();
-    // browser = await puppeteer.launch({
-    //   headless: false,
-    //   slowMo: 250, // slow down by 250ms
-    //   ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
-    // });
-    page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
-    await page.waitForSelector(".event");
-  });
-
-  afterAll(() => {
-    browser.close();
-  });
-
-  test("An event element is collapsed by default", async () => {
-    const eventDetails = await page.$(".event .extra-details.hide");
-    expect(eventDetails).toBeDefined();
-  });
-
-  test("User can expand an event to see its details", async () => {
-    await page.evaluate(() =>
-      document.querySelector(".show-details-btn").click()
-    );
-    const eventDetails = await page.$(".event .extra-details.show");
-    expect(eventDetails).toBeDefined();
-  });
-
-  test("User can collapse an event to hide its details", async () => {
-    await page.evaluate(() =>
-      document.querySelector(".hide-details-btn").click()
-    );
-    const eventDetails = await page.$(".event .extra-details.hide");
-    expect(eventDetails).toBeDefined();
-  });
-});
+// Feature 1
 
 describe("filter events by city", () => {
   let browser;
@@ -80,5 +40,48 @@ describe("filter events by city", () => {
     await page.evaluate(
       () => document.querySelector(".city").value === "Berlin, Germany"
     );
+  });
+
+  // Feature 2
+  describe("show/hide an event details", () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+      jest.setTimeout(30000);
+      browser = await puppeteer.launch();
+      // browser = await puppeteer.launch({
+      //   headless: false,
+      //   slowMo: 250, // slow down by 250ms
+      //   ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
+      // });
+      page = await browser.newPage();
+      await page.goto("http://localhost:3000/");
+      await page.waitForSelector(".event");
+    });
+
+    afterAll(() => {
+      browser.close();
+    });
+
+    test("An event element is collapsed by default", async () => {
+      const eventDetails = await page.$(".event .extra-details.hide");
+      expect(eventDetails).toBeDefined();
+    });
+
+    test("User can expand an event to see its details", async () => {
+      await page.evaluate(() =>
+        document.querySelector(".show-details-btn").click()
+      );
+      const eventDetails = await page.$(".event .extra-details.show");
+      expect(eventDetails).toBeDefined();
+    });
+
+    test("User can collapse an event to hide its details", async () => {
+      await page.evaluate(() =>
+        document.querySelector(".hide-details-btn").click()
+      );
+      const eventDetails = await page.$(".event .extra-details.hide");
+      expect(eventDetails).toBeDefined();
+    });
   });
 });
