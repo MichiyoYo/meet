18d027f4ceb62a1c2880cc28f81b960561b4f819
Cristina Lester <cristina.deelester@gmail.com>
2021-10-19 15:04:17 -0700

    added alert component

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
@@ -9,13 +10,22 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: `I can't find the location "${value}". Please try again.`,
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
@@ -31,6 +41,7 @@ class CitySearch extends Component {
       <div className="city-search-wrapper">
         <h2>Search</h2>
         <div className="CitySearch">
+          <InfoAlert text={this.state.infoText} />
           <input
             type="text"
             className="city"
