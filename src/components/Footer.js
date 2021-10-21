11d228be37c2830591b563b48a5dca93ac4abb36
Cristina Lester <cristina.deelester@gmail.com>
2021-10-21 15:25:17 -0700

    fixed position of footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,20 @@ function Footer(props) {
   return (
     <footer>
       <Row>
-        <Col sm={12} md={6}>
+        <Col sm={12} md={4} className="footer-slot">
           <p className="left">
             Designed and Developed by{" "}
             <a href="https://cristinalester.rocks">Cristina Lester</a>
           </p>
         </Col>
-
-        <Col sm={12} md={6}>
+        <Col sm={12} md={4} className="footer-slot">
+          <p>
+            <a href="https://michiyoyo.github.io/meet/privacy.html">
+              Privacy Policy
+            </a>
+          </p>
+        </Col>
+        <Col sm={12} md={4} className="footer-slot">
           <p>2021 &copy; All Rights Reserved</p>
         </Col>
       </Row>
